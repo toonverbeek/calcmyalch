920c1b206315d551a11cf5621ffe13334b8d2d37
Toon Verbeek <toon.verbeek1@gmail.com>
2015-03-22 16:18:30 +0100

    using rs csv api

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/go-martini/martini"
 )
@@ -19,30 +20,44 @@ type Skill struct {
 	CurrentXP int    `json:"experience"`
 	Level     int    `json:"level"`
 	Rank      int    `json:"rank"`
-	ID        int    `json:"id"`
-}
-
-type APISkill struct {
-	data []Skill `json:"data"`
 }
 
 func getProfileHighscore(handle string) {
-	resp, err := http.Get("http://silabsoft.org/rs-web/highscore/tonnu")
+	resp, err := http.Get("http://services.runescape.com/m=hiscore/index_lite.ws?player=tonnu")
 	reader := csv.NewReader(resp.Body)
 	reader.FieldsPerRecord = -1
-
 	cvsData, err := reader.ReadAll()
-	fmt.Printf("%+v\n", cvsData)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// var skill Skill
-	// var skills []Skill
-	for _, each := range cvsData {
-		fmt.Println(each[1])
-	}
+	csvToSkill(cvsData)
 
+}
+
+func csvToSkill(cvsData [][]string) {
+
+	var skills []*Skill
+
+	for x, each := range cvsData {
+		if len(each) == 3 {
+			if each[1] != "-1" && each[2] != "-1" {
+				if x == 7 {
+					rank, _ := strconv.Atoi(each[0])
+					level, _ := strconv.Atoi(each[1])
+					xp, _ := strconv.Atoi(each[2])
+					skill := &Skill{
+						Name:      "magic",
+						CurrentXP: xp,
+						Level:     level,
+						Rank:      rank,
+					}
+					skills = append(skills, skill)
+					fmt.Println(each[1], each[2])
+				}
+			}
+		}
+	}
 }
 
 func main() {
